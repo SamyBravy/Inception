2959ea110d94ef88b8849c9a039d54eadcaea980
Samuele <samybravy@gmail.com>
2025-02-11 00:34:47 +0100

    tolti commenti

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# if [ ! -f /var/www/html/wp-config.php ]; then
-#     wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --dbprefix=$DB_PREFIX --allow-root
-#     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
-# fi
 set -e
 
 WP_PATH=/var/www
