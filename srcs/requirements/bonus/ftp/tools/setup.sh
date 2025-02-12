a0611533d35b8c4d68a6867ecfd71604bfcae43d
SamyBravy <samybravy@gmail.com>
2025-02-12 12:49:05 +0100

    FINE

---
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if id -u "$FTP_USER" >/dev/null 2>&1; then
+	echo "Ready to go!"
+else
+	useradd -m $FTP_USER
+
+	echo  $FTP_USER:$FTP_PASS | /usr/sbin/chpasswd
+
+    echo "\e[31mciao, sono Samuele Dell'Erba e sono vegano\e[0m"
+
+	chmod -R 755 /var/www
+
+	chown -R $FTP_USER:$FTP_USER /var/www
+
+	echo "${FTP_USER}" >> /etc/vsftpd.userlist
+
+	mkdir -p /var/run/vsftpd/empty
+fi
+
+vsftpd /etc/vsftpd.conf
--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eux
 
 WP_PATH=/var/www
 
